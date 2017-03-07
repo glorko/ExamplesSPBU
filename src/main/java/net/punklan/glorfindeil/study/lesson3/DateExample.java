e7df36259aba4adfd666f4a2a7aa2faa10eb93bb
Aleksandr_Kichev <kichev.alexander@gmail.com>
2017-03-07 18:48:02 +0300

    new task + date examples

--- src/main/java/net/punklan/glorfindeil/study/lesson3/DateExample.java
+++ src/main/java/net/punklan/glorfindeil/study/lesson3/DateExample.java
@@ -13,27 +13,48 @@ import java.util.TimeZone;
  */
 public class DateExample {
     public static void main(String[] args) throws ParseException {
+        //get current date
+        //date is always in current timezone
         Date now = new Date();
+        System.out.println("Now is " + now);
+        Date zero = new Date(0);
+        System.out.println("Zero with current timezone is " + zero);
         Calendar anotherNow = Calendar.getInstance();
-        System.out.println(now);
-        System.out.println(anotherNow);
-        System.out.println(anotherNow.getTime());
-        System.out.println(anotherNow.getTimeInMillis());
+
+        System.out.println("Now calendar is " + anotherNow);
+        System.out.println("Now to date " + anotherNow.getTime());
+        System.out.println("Now in millis. " + anotherNow.getTimeInMillis());
         //todo date in another timezone
-        //работаем с таймзонами
-        Calendar nowInNewYork = Calendar.getInstance(
-                TimeZone.getTimeZone("New York"), new Locale("en", "US"));
-        System.out.println(nowInNewYork.getTime());
+
         DateFormat format = new SimpleDateFormat("dd.MM.yyyy");
         Date parsedDate = format.parse("20.02.1984");
-        System.out.println(parsedDate);
-        System.out.println(format.format(now));
-        //бегаем
+        System.out.println("Parsed date from date format" + parsedDate);
+        System.out.println("Printed today in date format" + format.format(now));
+        //бегаем по датам
         anotherNow.add(Calendar.DAY_OF_MONTH, 12);
         anotherNow.add(Calendar.MINUTE, 30);
-        System.out.println(anotherNow.getTime());
-        System.out.println(now.getMinutes());
+        System.out.println("Added time "+anotherNow.getTime());
+        //deprecated
+        System.out.println("Deprecated minutes" + now.getMinutes());
+        //correct
         anotherNow.setTime(now);
-        System.out.println(anotherNow.get(Calendar.MINUTE));
+        System.out.println("Correct minutes "+anotherNow.get(Calendar.MINUTE));
+        //работаем с таймзонами
+        Calendar nowInNewYork = Calendar.getInstance(
+                TimeZone.getTimeZone("America/New_York"), new Locale("en", "US"));
+        System.out.println("Hour in america: " + nowInNewYork.get(Calendar.HOUR_OF_DAY));
+
+        //convert full date
+        SimpleDateFormat sdfAmerica = new SimpleDateFormat();
+        TimeZone tzInAmerica = TimeZone.getTimeZone("America/New_York");
+        sdfAmerica.setTimeZone(tzInAmerica);
+
+        String sDateInAmerica = sdfAmerica.format(now); // Convert to String first
+        Date dateInAmerica = sdfAmerica.parse(sDateInAmerica); // Create a new Date object
+
+        System.out.println("TimeZone : " + tzInAmerica.getID() + " - " + tzInAmerica.getDisplayName());
+        System.out.println("TimeZone : " + tzInAmerica);
+        System.out.println("Date (New York) (String) : " + sDateInAmerica);
+        System.out.println("Date (New York) (Object) : " + sdfAmerica.format(dateInAmerica));
     }
 }
--- src/main/java/net/punklan/glorfindeil/study/lesson3/DateJava8Example.java
+++ src/main/java/net/punklan/glorfindeil/study/lesson3/DateJava8Example.java
@@ -1,7 +1,50 @@
 package net.punklan.glorfindeil.study.lesson3;
 
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+
+import static java.time.DayOfWeek.WEDNESDAY;
+import static java.time.temporal.ChronoUnit.MINUTES;
+import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
+import static java.time.temporal.TemporalAdjusters.previousOrSame;
+
 /**
  * Created by glorfindeil on 07.03.17.
  */
 public class DateJava8Example {
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now(
+        ));     // The current date and time
+        System.out.println(LocalDate.of(2012, Month.DECEMBER, 12)); // from values
+        System.out.println(LocalDate.ofEpochDay(150));  // middle of 1970
+        System.out.println(LocalTime.of(17, 18)); // the train I took home today
+        System.out.println(LocalTime.parse("10:15:30")); // From a String
+
+
+        //бегаем по датам
+        System.out.println(LocalDateTime.now().plusHours(10));
+        System.out.println(LocalDateTime.now().plus(10, MINUTES));
+        LocalDateTime timePoint = LocalDateTime.now();
+        System.out.println(timePoint.with(lastDayOfMonth()));
+        System.out.println(timePoint.with(previousOrSame(WEDNESDAY)));
+
+        //timezones
+        //current date with another zone
+        ZoneId id = ZoneId.of("Europe/Paris");
+        ZonedDateTime zoned = ZonedDateTime.of(LocalDateTime.now(), id);
+        System.out.println("zoned = " + zoned);
+
+        zoned = ZonedDateTime.now().withZoneSameInstant(id);
+        System.out.println("Now in Paris " + zoned);
+        //Instant
+        Instant now = Instant.now();
+        LocalDateTime ldt = LocalDateTime.ofInstant(now, ZoneId.systemDefault());
+        System.out.printf("%s %d %d at %d:%d%n", ldt.getMonth(), ldt.getDayOfMonth(),
+                ldt.getYear(), ldt.getHour(), ldt.getMinute());
+        //comparing
+        System.out.println(now.isBefore(timePoint.toInstant(ZoneOffset.ofHours(3))));
+    }
+
+
 }
